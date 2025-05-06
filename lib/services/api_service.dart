bc9c6f1b53fca82ef0c0a6435c7849b50087d91b
dayeon <dayeonko@outlook.com>
2025-05-06 17:45:11 +0900

    added : finalDrawingJsonData

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -5,15 +5,15 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class ApiService {
   static final _baseUrl = dotenv.env['IP_ADDR'];
 
-  static Future<String> sendAllStrokes(List<Map<String, dynamic>> strokes) async {
-    final url = Uri.parse('$_baseUrl/upload');
+  static Future<String> sendAllStrokes(List<Map<String, dynamic>> allJsonData, List<Map<String, dynamic>> finalDrawingJsonData) async {
+    final url = Uri.parse('$_baseUrl/reconstruction/sendStrokeData');
     final headers = {'Content-Type': 'application/json'};
 
     try {
       final response = await http.post(
         url,
         headers: headers,
-        body: jsonEncode({"strokes": strokes}),
+        body: jsonEncode({"drawing": allJsonData, "finalDrawing": finalDrawingJsonData}),
       );
       return response.statusCode == 200
           ? '서버 전송 성공!'
